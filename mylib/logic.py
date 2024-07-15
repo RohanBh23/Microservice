e69774470f58a3f1e39571dc8e184cfec8787177
RohanBh23 <78695257+RohanBh23@users.noreply.github.com>
2024-07-15 08:06:38 +0000

    adding a new cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -7,3 +7,10 @@ def wiki(name="War Godess", length=1):
     my_wiki = wikipedia.summary(name, length)
 
     return my_wiki
+
+def search_wiki(name):
+
+    results = wikipedia.search(name)
+    return results
+
+
